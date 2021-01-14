fe59b0050d215c1e859101ff45495eec238c2b5f
백다미 <s20033@gsm.hs.kr>
2021-01-14 17:24:04 +0900

    Learn syntax

---
+++ ex01/syntax/string.js
@@ -0,0 +1,3 @@
+console.log("1" + "1");
+console.log("hi dami");
+console.log("12345678910111213141516171819".length);
---
+++ ex01/syntax/variable.js
@@ -0,0 +1,5 @@
+// var, let, const
+var a = 1;
+console.log(a);
+const b = 2;
+console.log(b);
