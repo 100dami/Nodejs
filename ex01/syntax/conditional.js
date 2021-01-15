897e0c4ba3d1c5f65548178175638631e08706b1
백다미 <s20033@gsm.hs.kr>
2021-01-15 19:06:55 +0900

    Use conditional syntax in console

--- ex01/syntax/conditional.js
+++ ex01/syntax/conditional.js
@@ -1,9 +1,16 @@
 // 조건문
+var args = process.argv;
+console.log(args[2]);
+//console.log(args);
+
 console.log("A");
 console.log("B");
-if (false) {
+
+if (args[2] === "1") {
     console.log("C1");
 } else {
     console.log("C2");
 }
 console.log("D");
+
+
