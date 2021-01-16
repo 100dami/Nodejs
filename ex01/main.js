50507de2c8057459ae6bc0586d7739dd63dbcd1f
백다미 <s20033@gsm.hs.kr>
2021-01-16 21:04:33 +0900

    make homepage

--- ex01/main.js
+++ ex01/main.js
@@ -6,7 +6,6 @@ var app = http.createServer(function (request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query; // ? 뒤에 부분을 가져오게 하는 parse()
     var pathname = url.parse(_url, true).pathname;
-    var title = (queryData.id);
 
     console.log(queryData.id); // id 출력
 
@@ -21,32 +20,64 @@ var app = http.createServer(function (request, response) {
     // }
 
     if (pathname === "/") {
-        fs.readFile(`data/${queryData.id}`, "utf-8", function (err, description) {
+        if (queryData.id === undefined) {
 
-            // template literal : " ` `" , " ${} "
-            var template = `        
-                    <!doctype html>
-                <html>
-                <head>
-                <title>WEB1 -${title}</title>
-                <meta charset="utf-8">
-                </head>
-                <body>
-                <h1><a href="/">WEB</a></h1>
-                <ol>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">JavaScript</a></li>
-                </ol>
-                <h2>${title}</h2>
-                <p>${description}</p>
-                </body>
-                </html> 
-                `;
-            response.writeHead(200);
-            // response.write("Hi Dami\n");
-            response.end(template); // 서버에 template 를 보여줌
-        });
+            fs.readFile(`data/${queryData.id}`, "utf-8", function (err, description) {
+                var title = "Welcome";
+                var description = "Hello, Node.js";
+                // template literal : " ` `" , " ${} "
+                var template = `        
+                        <!doctype html>
+                    <html>
+                    <head>
+                    <title>WEB1 -${title}</title>
+                    <meta charset="utf-8">
+                    </head>
+                    <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ol>
+                        <li><a href="/?id=HTML">HTML</a></li>
+                        <li><a href="/?id=CSS">CSS</a></li>
+                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ol>
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                    </body>
+                    </html> 
+                    `;
+                response.writeHead(200);
+                // response.write("Hi Dami\n");
+                response.end(template); // 서버에 template 를 보여줌
+            });
+        } else {
+            fs.readFile(`data/${queryData.id}`, "utf-8", function (err, description) {
+                var title = (queryData.id);
+                // template literal : " ` `" , " ${} "
+                var template = `        
+                        <!doctype html>
+                    <html>
+                    <head>
+                    <title>WEB1 -${title}</title>
+                    <meta charset="utf-8">
+                    </head>
+                    <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ol>
+                        <li><a href="/?id=HTML">HTML</a></li>
+                        <li><a href="/?id=CSS">CSS</a></li>
+                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ol>
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                    </body>
+                    </html> 
+                    `;
+                response.writeHead(200);
+                // response.write("Hi Dami\n");
+                response.end(template); // 서버에 template 를 보여줌
+            });
+        }
+
     } else {
         response.writeHead(404);
         response.end("Not Found");
