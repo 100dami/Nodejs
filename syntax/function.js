184d59a12d787c0897ff5bddf7b04dd2f9bba974
백다미 <s20033@gsm.hs.kr>
2021-01-17 17:49:48 +0900

    Make readable code

---
+++ syntax/function.js
@@ -0,0 +1,9 @@
+function f123() {
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(5);
+}
+
+console.log("Start");
+f123();
---
+++ syntax/function2.js
@@ -0,0 +1,8 @@
+console.log(Math.round(1.6));
+console.log(Math.round(2.4));
+
+function sum(first, second) { // parameter : 매개변수
+    return (first+second);
+}
+
+console.log(sum(2,5)); // argument : 인자
